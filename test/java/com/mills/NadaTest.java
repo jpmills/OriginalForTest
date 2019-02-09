f4486597147777c880db69743997f62b7d59ed50
John P Mills <jpmills@users.noreply.github.com>
2019-02-08 23:15:30 -0500

    Create NadaTest.java

---
+++ test/java/com/mills/NadaTest.java
@@ -0,0 +1,49 @@
+
+package com.mills;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple Nada app.
+ */
+public class NadaTest
+{
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testNadaConstructor() {
+        try {
+            new Nada();
+        } catch (Exception e) {
+            fail("Construction failed.");
+        }
+    }
+
+    @Test
+    public void testNadaMain()
+    {
+        Nada.main(null);
+        try {
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+        } catch (AssertionError e) {
+            fail("\"message\" is not \"Hello World!\"");
+        }
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+}
